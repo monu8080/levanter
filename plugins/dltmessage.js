6e737a0fac837ba5b1f4bf602fadbab462aeefbd
monu8080 <rngrng581@gmail.com>
2025-03-05 18:12:22 +0530

    Create dltmessage.js

---
+++ plugins/dltmessage.js
@@ -0,0 +1,12 @@
+const lastMsgInChat = await getLastMessageInChat(jid) // implement this on your end
+await sock.chatModify({
+        delete: true,
+        lastMessages: [
+            {
+                key: lastMsgInChat.key,
+                messageTimestamp: lastMsgInChat.messageTimestamp
+            }
+        ]
+    },
+    jid
+)
